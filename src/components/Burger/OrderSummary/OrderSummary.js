93dcd9c8d124b392147d361b79686550d29fb7d2
oil <saritaoil@hotmail.com>
2018-12-20 10:38:27 +0700

    "edit auxx file"

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import Aux from "../../../hoc/Auxx/Aux";
+import Aux from "../../../hoc/Auxx/Auxx";
 import Button from "../../UI/Button/Button";
 
 class OrderSummary extends Component {
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { Route } from "react-router-dom";
 import {connect} from 'react-redux';
-import Aux from "../../hoc/Auxx/Aux";
+import Aux from "../../hoc/Auxx/Auxx";
 import Burger from "../../components/Burger/Burger";
 import BuildControls from "../../components/Burger/BuildControls/BuildControls";
 import Modal from "../../components/UI/Modal/Modal";
